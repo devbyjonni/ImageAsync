d454446c33480bef5bd3a2977eb3c05de7f521ca
Jonni Åkesson <enterthesource@me.com>
2024-09-09 01:47:46 +0200

    Initial Commit

---
+++ ImageAsync/ImageAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAsyncApp.swift
+//  ImageAsync
+//
+//  Created by Jonni Akesson on 2024-09-09.
+//
+
+import SwiftUI
+
+@main
+struct ImageAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ ImageAsync/Views/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  ImageAsync
+//
+//  Created by Jonni Akesson on 2024-09-09.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, HomeView!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
