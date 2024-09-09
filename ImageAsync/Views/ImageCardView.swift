56c8c6c9a660f8cac7c3a4cdadfd460d4e0b299c
Jonni Åkesson <enterthesource@me.com>
2024-09-09 02:06:22 +0200

    feat: add ImageCardView for displaying images with metadata
    - Created `ImageCardView` to showcase images alongside author metadata.
    - Integrated `ImageLoaderView` to load images dynamically using the provided loader type.
    - Added fallback text for when no valid image URL is available.
    - Used `GeometryReader` for flexible layout and dynamic sizing of images.

---
+++ ImageAsync/Views/ImageCardView.swift
@@ -0,0 +1,37 @@
+//
+//  ImageCardView.swift
+//  ImageAsync
+//
+//  Created by Jonni Akesson on 2024-09-09.
+//
+
+import SwiftUI
+
+struct ImageCardView: View {
+    var imageModel: ImageModel
+    var loaderType: ImageLoaderType
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let size = geometry.size
+            VStack(alignment: .leading, spacing: 10, content: {
+                if let validUrl = imageModel.imageURL {
+                    ImageLoaderView(imageUrl: validUrl, loaderType: loaderType, size: size)
+                } else {
+                    Text("No image available")
+                        .foregroundStyle(.gray)
+                        .font(.caption)
+                }
+                Text(imageModel.author)
+                    .font(.caption)
+                    .foregroundStyle(.gray)
+                    .lineLimit(1)
+            })
+        }
+        .frame(height: 200)
+    }
+}
+
+#Preview {
+    HomeView()
+}
