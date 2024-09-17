6265e9a3f50af62f07d0512fbe7903ecdefedd4d
Jonni Åkesson <enterthesource@me.com>
2024-09-18 00:41:33 +0200

    ref(PicsumPhoto): Refactor PicsumPhoto struct to class with additional properties
    - Refactor PicsumPhoto from a struct to a class to support additional mutable properties.
    - Added `isFavorite` and `savedAt` properties for managing photo favorites and saving dates.
    - Updated initializer to include default values for `isFavorite` and `savedAt`.
    - Implemented `Codable` conformance for custom encoding and decoding logic.
    - Maintained `photoURL` computed property for URL construction.

--- PhotoFetcher/Models/PicsumPhoto.swift
+++ PhotoFetcher/Models/PicsumPhoto.swift
@@ -1,9 +1,3 @@
-//
-//  PicsumPhoto.swiftd
-//  PhotoFetcher
-//
-//  Created by Jonni Akesson on 2024-09-09.
-//
 
 import Foundation
 
@@ -19,18 +13,21 @@ import Foundation
  }
  */
 
-struct PicsumPhoto: Identifiable, Codable {
-    let id: String
-    let author: String
-    let url: String
-    let downloadURL: String
-
-    // Add a manual initializer for testing (XCTestCase)
-    init(id: String, author: String, url: String, downloadURL: String) {
+class PicsumPhoto: Identifiable, Codable {
+    var id: String
+    var author: String
+    var url: String
+    var downloadURL: String
+    var isFavorite: Bool = false
+    var savedAt: Date = Date()
+    
+    init(id: String, author: String, url: String, downloadURL: String, isFavorite: Bool = false, savedAt: Date = Date()) {
         self.id = id
         self.author = author
         self.url = url
         self.downloadURL = downloadURL
+        self.isFavorite = isFavorite
+        self.savedAt = savedAt
     }
     
     private enum CodingKeys: String, CodingKey {
@@ -42,6 +39,22 @@ struct PicsumPhoto: Identifiable, Codable {
     
     var photoURL: URL? {
         URL(string: "https://picsum.photos/id/\(id)/256/256.jpg")
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        author = try container.decode(String.self, forKey: .author)
+        url = try container.decode(String.self, forKey: .url)
+        downloadURL = try container.decode(String.self, forKey: .downloadURL)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(author, forKey: .author)
+        try container.encode(url, forKey: .url)
+        try container.encode(downloadURL, forKey: .downloadURL)
     }
 }
 
